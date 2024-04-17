bc3addfcf383e6d38f82aa3a252892548981d249
Moncef Abboud <moncef.abboud95@gmail.com>
2024-04-17 17:23:12 +0200

    feat: remove links from AuthenticatedUserDropdown

--- src/learning-header/AuthenticatedUserDropdown.jsx
+++ src/learning-header/AuthenticatedUserDropdown.jsx
@@ -31,14 +31,6 @@ const AuthenticatedUserDropdown = ({ intl, username, name }) => {
           <Dropdown.Item href={`${getConfig().ACCOUNT_PROFILE_URL}/u/${username}`}>
             {intl.formatMessage(messages.profile)}
           </Dropdown.Item>
-          <Dropdown.Item href={getConfig().ACCOUNT_SETTINGS_URL}>
-            {intl.formatMessage(messages.account)}
-          </Dropdown.Item>
-          { getConfig().ORDER_HISTORY_URL && (
-            <Dropdown.Item href={getConfig().ORDER_HISTORY_URL}>
-              {intl.formatMessage(messages.orderHistory)}
-            </Dropdown.Item>
-          )}
           <Dropdown.Item href={getConfig().LOGOUT_URL}>
             {intl.formatMessage(messages.signOut)}
           </Dropdown.Item>
