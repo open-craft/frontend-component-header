a149236e83d1988768494c4e0d220820a070e830
Fox Danger Piacenti <fox@opencraft.com>
2025-07-11 14:35:21 -0500

    Paragon sampling.

--- example/index.js
+++ example/index.js
@@ -8,6 +8,16 @@ import Header from '@edx/frontend-component-header';
 
 import './index.scss';
 import StudioHeader from '../src/studio-header/StudioHeader';
+import {Alert, Button, Card, Col, Container, Icon, Navbar, Row} from "@openedx/paragon";
+import {faCheckCircle, faWarning, faInfo, faStop} from "@fortawesome/free-solid-svg-icons";
+
+const variants = ["primary", "secondary", "brand"];
+const alertVariants = [
+  {label: "success", icon: faCheckCircle},
+  {label: "danger", icon: faStop},
+  {label: "info", icon: faInfo},
+  {label: "warning", icon: faWarning},
+]
 
 subscribe(APP_READY, () => {
   ReactDOM.render(
@@ -17,51 +27,48 @@ subscribe(APP_READY, () => {
         authenticatedUser: null,
         config: getConfig(),
       }}>
-        <Header />
+        <Container>
+          <div className="my-2">
+            {variants.map((variant) => <Button variant={variant} className="mx-2">{variant}</Button>)}
+          </div>
+          <div className="my-2">
+            {alertVariants.map((variant) => (
+              <Alert variant={variant.label} className="mx-2">
+                <Alert.Heading>{variant.label}</Alert.Heading>
+                <p>This is a {variant.label} message.</p>
+              </Alert>
+            ))}
+          </div>
+          <Row>
+            <Col md={4} lg={3}>
+              <Card>
+                <Card.ImageCap
+                  src="https://picsum.photos/360/200/"
+                  srcAlt="Card image"
+                />
+                <Card.Header title="Card Title" />
+                <Card.Section>
+                  <p className="text-accent-a">Accent A text!</p>
+                </Card.Section>
+              </Card>
+            </Col>
+            <Col md={4} lg={3}>
+              <Card>
+                <Card.ImageCap
+                  src="https://picsum.photos/360/200/"
+                  srcAlt="Card image"
+                />
+                <Card.Header title="Card Title" />
+                <Card.Section className="bg-accent-b">
+                  <p>Accent B background.</p>
+                </Card.Section>
+              </Card>
+            </Col>
+          </Row>
+          <Navbar bg="light">Light Navbar</Navbar>
+          <Navbar bg="dark"><span className="text-light">Dark Navbar</span></Navbar>
+        </Container>
       </AppContext.Provider>
-      <h5 className="mt-2 mb-5">Logged out state</h5>
-
-      {/* We can fake out authentication by including another provider here with the data we want */}
-      <AppContext.Provider value={{
-        authenticatedUser: {
-          userId: '123abc',
-          username: 'testuser',
-          roles: [],
-          administrator: false,
-        },
-        config: getConfig(),
-      }}>
-        <Header />
-      </AppContext.Provider>
-      <h5 className="mt-2  mb-5">Logged in state</h5>
-      <AppContext.Provider value={{
-        authenticatedUser: {
-          userId: '123abc',
-          username: 'testuser',
-          roles: [],
-          administrator: false,
-        },
-        config: getConfig(),
-      }}>
-        <StudioHeader
-          number="run123"
-          org="testX"
-          title="Course Name"
-          isHiddenMainMenu={false}
-          mainMenuDropdowns={[
-            {
-              id: 'content-dropdown',
-              buttonTitle: 'Content',
-              items: [{
-                href: '#',
-                title: 'Outline',
-              }],
-            },
-          ]}
-          outlineLink="#"
-        />
-      </AppContext.Provider>
-      <h5 className="mt-2">Logged in state for Studio header</h5>
     </AppProvider>,
     document.getElementById('root'),
   );
